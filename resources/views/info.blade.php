257fbd4be2925551abbdb93a7aab9cdc20dbdd73
ebenhezer7 <tersakamado80@gmail.com>
2023-08-09 22:19:53 +0700

    tugas coba coba pa egi
    mencoba admin lte terbaru

---
+++ resources/views/info.blade.php
@@ -0,0 +1,16 @@
+@extends('adminlte')
+@section('content')
+<section class="section">
+    <div class="section-header">
+        <h1>Informational Page</h1>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h4>ini judul halaman</h4>
+        </div>
+        <div class="card-body">
+            <p>ini isi halaman</p>
+        </div>
+    </div>
+</section>
+@endsection
